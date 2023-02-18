7352a3ba2ab53f6bd7b5b3886c0cb1f7624f9da2
Leonardo Gomes da Silva <leogomesdev@gmail.com>
2023-02-18 16:37:02 -0500

    feat(home): add Home page, protected by authentication

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import { AuthProvider } from "./auth/AuthContext";
 import Login from "./components/Login";
+import PrivateRoute from "./PrivateRoute";
+import Home from "./components/Home";
 
 const App: React.FC = () => {
   return (
     <AuthProvider>
       <Router>
         <Routes>
+          <Route element={<PrivateRoute />}>
+            <Route path="/" element={<Home />} />
+          </Route>
           <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
---
+++ src/components/Home.tsx
@@ -0,0 +1,17 @@
+import { useContext } from "react";
+import { AuthContext } from "../auth/AuthContext";
+
+const Home: React.FC = () => {
+  const { handleLogout } = useContext(AuthContext);
+
+  return (
+    <div>
+      <h1>Logged in</h1>
+      <p>If you want to see the sunshine, you have to weather the storm</p>
+
+      <button onClick={handleLogout}>Logout</button>
+    </div>
+  );
+};
+
+export default Home;
